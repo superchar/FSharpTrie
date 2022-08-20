f430150e41d14ae227c732270d16b82b17adfec8
Vladyslav Lobyntsev <lobintsevvladislav@gmail.com>
2022-08-20 19:20:41 +0300

    Added getStrings

--- FSharpTrie/trie.fs
+++ FSharpTrie/trie.fs
@@ -1,10 +1,10 @@
 ﻿module Trie
 
 type TrieNode = {
-    IsComplete: bool; Children: Map<char, TrieNode>
+    KeyPart: char; IsComplete: bool; Children: Map<char, TrieNode>
 }
 
-let Root = { IsComplete = false; Children = Map.empty }
+let Root = { KeyPart = '0'; IsComplete = false; Children = Map.empty }
 
 let private updateChild (keyPart: char) (node: TrieNode) (child: TrieNode) =
     { node with Children = node.Children |> Map.add keyPart child }
@@ -16,7 +16,7 @@ let rec private addRec (node: TrieNode) (key: list<char>): TrieNode =
                         Map.tryFind head with
                         | Some(child) -> updateChild head node (addRec child tail)
                         | None ->  updateChild head node
-                                       (addRec { IsComplete = tail = []; Children = Map.empty } tail)
+                                       (addRec { KeyPart = head; IsComplete = tail = []; Children = Map.empty } tail)
     | [] -> node
 
 let add (key: string) (node: TrieNode) = key
@@ -29,8 +29,7 @@ let rec private containsKeyRec(node: TrieNode) (key: list<char>) =
                         Map.tryFind head with
                         | Some(child) -> child.IsComplete
                         | None -> false 
-    | head :: tail ->
-        match node.Children |>
+    | head :: tail -> match node.Children |>
                         Map.tryFind head with
                         | Some(child) -> containsKeyRec child tail
                         | None -> false 
@@ -39,3 +38,15 @@ let rec private containsKeyRec(node: TrieNode) (key: list<char>) =
 let containsKey(key: string) (node: TrieNode) : bool = key
                                                        |> Seq.toList
                                                        |> containsKeyRec node
+
+let rec private getStringsRec(node: TrieNode): char list list =
+    if(node.Children.IsEmpty) then [[]]
+    else
+    let strings = (node.Children
+    |> Seq.toList
+    |> List.collect (fun kv -> getStringsRec(kv.Value) |> List.map(fun l -> kv.Key :: l)))
+    if node.IsComplete then [node.KeyPart] :: strings
+    else strings
+
+let getStrings(node: TrieNode): string list =
+    getStringsRec node |> List.map(fun w -> w |> List.toArray |> System.String)
